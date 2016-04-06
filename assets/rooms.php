1d742630da1906e263ef63a996d3200ef19db1f2
Abbus <akyukleyen@hotmail.com>
2016-04-06 22:53:35 +0200

    search werkt nu helemaal

--- assets/rooms.php
+++ assets/rooms.php
@@ -1,3 +1,71 @@
+<?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL); 
+
+
+  include "database.php";
+
+
+  if (isset($_POST['submit'])){
+    $sql_string = "INSERT INTO rooms (id, room_type, room_number, adults, children, beds, price, description) VALUES ('$_POST[id]','$_POST[room_type]','$_POST[room_number]','$_POST[adults]','$_POST[children]','$_POST[beds]','$_POST[price]','$_POST[description]')";
+    $result = mysqli_query($conn, $sql_string);
+  }
+
+
+  else if(isset($_POST['update'])){
+    $sql_string = "UPDATE rooms SET room_type =". $_POST['room_type'].
+    ", room_number=" . $_POST['room_number'].
+    ", adults=" . $_POST['adults'].
+    ", children=" . $_POST['children'].
+    ", beds=" . $_POST['beds'].
+    ", price=" . $_POST['price'].
+    ", description=" . $_POST['description'].
+    " where id = ". $_POST['id'];
+    $result = mysqli_query($conn, $sql_string);
+  }
+  
+  else if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql_string =  "DELETE FROM rooms WHERE id = '$id' ";
+    mysqli_query($conn, $sql_string);
+  }
+
+
+  $sql_string = "SELECT * FROM rooms";
+  $roomData = mysqli_query($conn, $sql_string);
+
+
+  echo "<table>
+        <tr>
+        <th> room type </th>
+        <th> room number </th>
+        <th> adults </th>
+        <th> children </th>
+        <th> beds </th>
+        <th> price </th>
+        <th> description </th>
+        </tr>";
+
+  while ($record = mysqli_fetch_array($roomData)) {
+    echo "<tr>";
+    echo "<td>" . $record['room_type'] . "</td>"; 
+    echo "<td>" . $record['room_number'] . "</td>";
+    echo "<td>" . $record['adults'] . "</td>";
+    echo "<td>" . $record['children'] . "</td>";
+    echo "<td>" . $record['beds'] . "</td>";
+    echo "<td>" . $record['price'] . "</td>";
+    echo "<td>" . $record['description'] . "</td>";
+    echo "<td>" . "<a href='rooms.php?id=" . $record['id'] . " ' name='delete'> delete </a></td>";
+    echo "<td>" . "<a href='update.php?id=" . $record['id'] . " ' name='update'> edit </a></td>";
+    echo "</tr>";
+  }
+
+  echo "</table>";
+
+  $conn->close();
+?>
+
 <html>
 <head>
 <title>Hotel Booker Rooms</title>
@@ -5,10 +73,10 @@
 <body>
 
 <h1>Add a room:</h1> 
-  <form action="" method="post">
+  <form action="rooms.php" method="post">
     <p>
-        Floor:<br>
-        <input type="number" name="floor">
+        Room type:<br>
+        <input type="text" name="room_type">
     </p>
 
     <p>
@@ -31,70 +99,18 @@
         <input type="number" name="beds">
     </p>
 
+    <p>
+        Price:<br>
+        <input type="number" name="price">
+    </p>
+    
+    <p>
+        Description:<br>
+        <input type="text" name="description">
+    </p>
+
     <input type="submit" name="submit">
   </form>
 
-<?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL); 
-
-
-  include "database.php";
-
-
-  if (isset($_POST['submit'])){
-    $sql_string = "INSERT INTO rooms (id, floor, room_number, adults, children, beds) VALUES ('$_POST[id]','$_POST[floor]','$_POST[room_number]','$_POST[adults]','$_POST[children]','$_POST[beds]')";
-    $result = mysqli_query($conn, $sql_string);
-  }
-
-  else if(isset($_POST['update'])){
-    $sql_string = "UPDATE rooms SET floor =". $_POST['floor'].
-    ", room_number=" . $_POST['room_number'].
-    ", adults=" . $_POST['adults'].
-    ", children=" . $_POST['children'].
-    ", beds=" . $_POST['beds'].
-    " where id = ". $_POST['id'];
-    $result = mysqli_query($conn, $sql_string);
-  }
-  
-  else if (isset($_GET['id'])){
-    $id = $_GET['id'];
-    $sql_string =  "DELETE FROM rooms WHERE id = '$id' ";
-    mysqli_query($conn, $sql_string);
-  }
-
-
-  $sql_string = "SELECT * FROM rooms";
-  $roomData = mysqli_query($conn, $sql_string);
-
-
-  echo "<table>";
-  echo "<tr>";
-  echo "<th>" . "floor" . "</th>";
-  echo "<th>" . "room number" . "</th>";
-  echo "<th>" . "adults" . "</th>";
-  echo "<th>" . "children" . "</th>";
-  echo "<th>" . "beds" . "</th>";
-  echo "</tr>";
-
-  while ($record = mysqli_fetch_array($roomData)) {
-    echo "<tr>";
-    echo "<td>" . $record['floor'] . "</td>"; 
-    echo "<td>" . $record['room_number'] . "</td>";
-    echo "<td>" . $record['adults'] . "</td>";
-    echo "<td>" . $record['children'] . "</td>";
-    echo "<td>" . $record['beds'] . "</td>";
-    echo "<td>" . "<a href='rooms.php?id=" . $record['id'] . " ' name='delete'> delete  </a>" . "</td>";
-    echo "<td>" . "<a href='update.php?id=" . $record['id'] . "&floor=" . $record['floor'] . "&room_number=" . $record['room_number'] . " ' name='update'> edit </a>" . "</td>";
-    echo "<td>" . "<a href='search.php?adults=" . $record['adults'] . "&children=" . $record['children'] . " ' name='search'> search </a>" . "</td>";
- 
-    echo "</tr>";
-  }
-
-  echo "</table>";
-
-  $conn->close();
-?>
 </body>
 </html>
--- assets/search.php
+++ assets/search.php
@@ -1,4 +1,7 @@
 <?php 
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL); 
 
 
 function search(){
@@ -8,24 +11,36 @@ function search(){
     if (isset($_POST['search'])){
 
         $adults = $_POST['adults'];
-        $
-        $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+        $children = $_POST['children'];
+        //$beds = $_POST['beds']
 
-        $sql_string = "SELECT * FROM rooms WHERE adults LIKE '$' ";
-        $roomData = mysqli_query($conn, $sql_string) or die ("could not search!");
+       // $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+
+        $sql_string = "SELECT * FROM rooms WHERE adults LIKE '%$adults%' AND children LIKE '%$children%' ";
+        $result = mysqli_query($conn, $sql_string);
+        $count = mysqli_num_rows($result);
         
-        $record = mysqli_fetch_array($roomData);
-
-
-
+        if ($count == 0){
+            echo "There was no search results!";
+        }
+        else {
+            while ($record = mysqli_fetch_array($result)) {
+                echo "<div class='room'>";
+                echo "<div class='images'><img src='../img/kingroom.jpg' alt='kingroom' height='275px'></div>";
+                echo "<div class='text'>";
+                echo "<h1>" . $record['room_type'] . "</h1>";
+                echo "<div class='rooms'><h2> Rooms: 1</div><div class='max'><h2>Adults: " . $record['adults'] . "</h2></div><div class='max'><h2>Children: " . $record['children'] . "</h2></div><br><br>";
+                echo "<div class='description'>" . $record['description'] . "</div>";
+                echo "<div style='margin-top: 160px'><div style='margin-top:0px' class='myButton'><a href='#' class='myButtonStyle'>BOOK NOW</a></div>";
+                echo "<div class='price'> $" . $record['price'] . "/NIGHT</div></div>";
+                echo "</div> </div>";
+            }
+        }
     }
     else {
         echo "No result for your search! Please try again.";
     }
-
-
 }
-
 
 ?>
 
@@ -34,12 +49,17 @@ function search(){
 <html>
 <head>
 <title>Hotel Booker Rooms</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../style/stylesearch.css">
 </head>
 <body>
 
-<h1>Gevonden kamers:</h1> 
 
-<?php search(); ?>
+<?php 
+search(); 
+
+
+?>
 
 
 </body>
